312c32943e7a656aeca82eb5f59c1a3fd579d121
sumitpahadi <sumitrawat1102000@gmail.com>
2023-09-20 12:39:16 +0530

    lazyloading

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,36 @@
-import Routercompo from "./Component/Routes/Routercompo";
 import "./App.css";
 import { Navbar } from "./Component/Navbar";
+import { lazy, Suspense } from "react";
 
 import Footer from "./Component/Footer";
 import Contextapi from "./Component/Contextapi/Contextapi";
 
+const Lazyloading = lazy(() => import("./Component/Routes/Routercompo"));
 
 function App() {
-  
   return (
     <div className="App">
       <Navbar />
 
-      <Contextapi >
-      <Routercompo/>
-        
+      <Contextapi>
+        <Suspense
+          fallback={
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                height: "100vh",
+              }}
+            >
+              <h1 style={{ color: "red", fontSize: "40px" }}>Loading...</h1>
+            </div>
+          }
+        >
+          <Lazyloading />
+        </Suspense>
       </Contextapi>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
