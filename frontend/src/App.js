c05975f72192aef58c8b83e8203a8c543d6fbb5a
sumitpahadi <sumitrawat1102000@gmail.com>
2023-09-20 16:17:00 +0530

    last commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,36 +1,26 @@
-import "./App.css";
-import { Navbar } from "./Component/Navbar";
 import { lazy, Suspense } from "react";
-
-import Footer from "./Component/Footer";
-import Contextapi from "./Component/Contextapi/Contextapi";
 
 const Lazyloading = lazy(() => import("./Component/Routes/Routercompo"));
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-
-      <Contextapi>
-        <Suspense
-          fallback={
-            <div
-              style={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                height: "100vh",
-              }}
-            >
-              <h1 style={{ color: "red", fontSize: "40px" }}>Loading...</h1>
-            </div>
-          }
-        >
-          <Lazyloading />
-        </Suspense>
-      </Contextapi>
-      <Footer />
+      <Suspense
+        fallback={
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              height: "100vh",
+            }}
+          >
+            <h1 style={{ color: "red", fontSize: "40px" }}>Loading...</h1>
+          </div>
+        }
+      >
+        <Lazyloading />
+      </Suspense>
     </div>
   );
 }
--- frontend/src/Component/Routes/Routercompo.js
+++ frontend/src/Component/Routes/Routercompo.js
@@ -1,5 +1,5 @@
 import { Route, Routes } from "react-router-dom";
-
+import "../App.css";
 import Bollywood from "../pages/Bollywood";
 
 import Technology from "../pages/Technology";
@@ -13,22 +13,29 @@ import Fitness from "../pages/Fitness";
 import Food from "../pages/Food";
 
 import Detaildesciption from "../Detaildesciption";
+import Contextapi from "../Contextapi/Contextapi";
+import { Navbar } from "../Navbar";
+import Footer from "../Footer";
 
 function Routercompo() {
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/bollywood" element={<Bollywood />} />
-        <Route path="/hollywood" element={<Hollywood />} />
-        <Route path="/technology" element={<Technology />} />
-        <Route path="/fitness" element={<Fitness />} />
-        <Route path="/food" element={<Food />} />
-        <Route
-          path="/detaildescription/:id/:catgory"
-          element={<Detaildesciption />}
-        />
-      </Routes>
+    <div className="App">
+      <Navbar></Navbar>
+      <Contextapi>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/bollywood" element={<Bollywood />} />
+          <Route path="/hollywood" element={<Hollywood />} />
+          <Route path="/technology" element={<Technology />} />
+          <Route path="/fitness" element={<Fitness />} />
+          <Route path="/food" element={<Food />} />
+          <Route
+            path="/detaildescription/:id/:catgory"
+            element={<Detaildesciption />}
+          />
+        </Routes>
+      </Contextapi>
+      <Footer></Footer>
     </div>
   );
 }
