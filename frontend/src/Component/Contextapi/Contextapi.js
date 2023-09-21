a017c42d64e00a87251dec52378f782962422226
sumitpahadi <sumitrawat1102000@gmail.com>
2023-09-21 21:17:33 +0530

    one last commit

--- frontend/src/Component/Contextapi/Contextapi.js
+++ frontend/src/Component/Contextapi/Contextapi.js
@@ -1,30 +1,3 @@
-// import React from "react";
-// import { createContext } from "react";
-// import axios from "axios";
-// import { useEffect, useState } from "react";
-// export const Contextdata = createContext();
-// const URL = "http://localhost:5000/home";
-// function Contextapi({childern}) {
-//   const [data, setData] = useState([]);
-//   useEffect(() => {
-//     axios.get(URL)
-//     .then((response)=>setData(response.data))
-//     .catch((error)=>console.log(error))
-//   }, []);
-//   console.log(data)
-//   return (
-//     <>
-//       {data && (
-//         <Contextdata.Provider value={data}>
-//           {childern} 
-//         </Contextdata.Provider>
-//       )}
-//     </>
-//   );
-// }
-
-// export default Contextapi;
-
 
 import React, { createContext, useEffect, useState } from "react";
 import axios from "axios";
